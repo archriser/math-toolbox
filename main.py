b8636cf77f79c644343f9984ce4a3466d13e7d55
Tasdik <mtasdikyousuf@gmail.com>
2024-12-25 04:55:23 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# basic calculator
+# prime number checker
+# gcd finder
+# lcm finder
+# palindrome checker with reversal output
+# Armstrong number and leap year checker
+# square root approximation
+# decimal to binary converter
+# Fibonacci sequence
+# Basic Statistics
+# Series Summation and digit sum calculator
+# area calculator
+# Matrix operator
+# Encryption
